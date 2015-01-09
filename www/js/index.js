f34f6a0e737d34668d6b337f9881c7dfd05d7433
pilot007 <selimgoktas@gmail.com>
2015-01-09 17:56:45 +0200

    ppp

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,15 @@
+var isThereNewCampain=true;
+
+// aeropostale
+var icon2=false;
+//bananerepuvlic
+var icon3=false;
+//gap
+var icon22=false;				    	  
+//mark spencer
+var icon27=false;
+
+var map;
 var lineChartData = {
  labels : ["","","","","","",""],
  datasets : [
@@ -70,27 +82,27 @@ var pieData = [
     legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
 
 }
-];		 
+];
 
 function initPushwoosh() {
-    var pushNotification = window.plugins.pushNotification;
-    if(device.platform == "Android")
-    {
-        registerPushwooshAndroid();
-    }
+	var pushNotification = window.plugins.pushNotification;
+	if(device.platform == "Android")
+	{
+		registerPushwooshAndroid();
+	}
 
-    if(device.platform == "iPhone" || device.platform == "iOS")
-    {
-        registerPushwooshIOS();
-    }
+	if(device.platform == "iPhone" || device.platform == "iOS")
+	{
+		registerPushwooshIOS();
+	}
 }
-
 var app = {
 	// Application Constructor
 	initialize : function() {
 		console.log("init");
+		google.load("maps", "3.8", {"callback": map, other_params: "sensor=true&language=en"});
 		this.bindEvents();
-		app.url="http://10.0.0.31:8080/fiba_group_webservices/";
+		//app.url="http://10.0.0.31:8080/fiba_group_webservices/";
 		app.total_points=0;
 		app.url="http://85.97.120.30:9090/fiba_group_webservices/";
 		//app.first_init();
@@ -105,40 +117,18 @@ var app = {
                             
 	},
 	onDeviceReady : function() {
-		console.log("ondevice ready");
-		  initPushwoosh();
 		app.receivedEvent('deviceready');
-		// navigator.geolocation.getCurrentPosition(app.onSuccess, app.onError);
+		console.log("ondevice ready");		
 		app.first_init();
+		try{
+			initPushwoosh();
+		}catch(err) 
+		{
+		   		console.log(err.message);
+		}
+
 	//new Chart(document.getElementById("line").getContext("2d")).Line(lineChartData);
-	  
-		
 	},
-	//google map start
-	// onSuccess: function(position){
-        // var longitude = position.coords.longitude;
-        // var latitude = position.coords.latitude;
-        // var latLong = new google.maps.LatLng(latitude, longitude);
-//  
-        // var mapOptions = {
-            // center: latLong,
-            // zoom: 13,
-            // mapTypeId: google.maps.MapTypeId.ROADMAP
-        // };
-//  
-        // var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-//     
-        // var marker = new google.maps.Marker({
-              // position: latLong,
-              // map: map,
-              // title: 'my location'
-          // });
-    // },
-//     
-    // onError: function(error){
-        // alert("the code is " + error.code + ". \n" + "message: " + error.message);
-    // },
-    //google map end
 	// Update DOM on a Received Event
 	receivedEvent : function(id) {
 		console.log("receive event");
@@ -151,15 +141,17 @@ var app = {
 		
 		//checkConnection();
 	},
-	productList : null,
 	fnc_Barkod : function() {
 				$("#un_barkod").empty();
-		        $("#un_barkod").append(app.user_name);
+		        $("#un_barkod").append(app.user_name + "("+app.total_points+")");
+		        
+				$("#barkod_id").empty();
+				$("#barkod_id").append(app.id);
+		        
 	},	
 	fnc_Puanlarim : function() {
 				$("#un_puanlarim").empty();
-		        $("#un_puanlarim").append(app.user_name);
-		        console.log("puanlarım 1");
+		        $("#un_puanlarim").append(app.user_name + "("+app.total_points+")");		        
 		$.ajax({
 			url : app.url+"GetAcitivies?member_id="+app.id,
 			dataType : "json",
@@ -171,6 +163,13 @@ var app = {
 				console.log("puanlarım 3");
 				 
 				
+				html ="<table style='width:100%'>";
+					html += '<tr><td width="50%">Şirket Adı</td>';
+					html += '<td width="30%">İşlem Tarihi</td>';
+					html += '<td width="20%">Kazanılan Puan</td></tr>';
+				    html+="</table>";
+				listItems.append('<li id="prj_header">' + html + '</li>');
+								
 				for (var i = 0; i < a.length; i++) {
 				html ="<table style='width:100%'>";
 					console.log("puanlarım 4");
@@ -208,25 +207,97 @@ var app = {
 	fnc_Mesajlar : function() {
 				$("#un_mesajlar").empty();
 		        $("#un_mesajlar").append(app.user_name + "("+app.total_points+")");
+		        
+		$.ajax({
+			url : app.url+"GetCampains?member_id="+app.id,
+			dataType : "json",
+			success : function(a, b, c) {
+				console.log("kampanyalar");
+				$('#div_mesajlar ul').remove();
+				$('#div_mesajlar').append('<ul data-role="listview"></ul>');
+				listItems = $('#div_mesajlar').find('ul');
+				console.log("div_mesajlar 3");
+				 
+				html ="<table style='width:100%'>";
+					html += '<tr><td width="25%"> Şirket Adı </td>';
+					html += '<td width="30%"> Kampanya </td>';
+					html += '<td width="15%">İndirim Oranı</td>';
+					//html += '<td width="15%">' + a[i].startdate + '</td>';
+					html += '<td width="15%"> Kampanya Bitiş Tarihi</td></tr>';
+				    html+="</table>";
+				listItems.append('<li id="prj_header">' + html + '</li>');
+				for (var i = 0; i < a.length; i++) {
+				html ="<table style='width:100%'>";
+					console.log("div_mesajlar 4");
+					html += '<tr><td width="25%">'+ a[i].company_name+ '</td>';
+					html += '<td width="30%">' + a[i].campain_name + '</td>';
+					html += '<td width="15%">' + a[i].discount + '</td>';
+					//html += '<td width="15%">' + a[i].startdate + '</td>';
+					html += '<td width="15%">' + a[i].expiredate + '</td></tr>';
+				    html+="</table>";
+					listItems.append('<li id="prj_' + a[i].campain_id + '">' + html + '</li>');
+				};
+				
+				$('#div_mesajlar ul').listview();
+				console.log("div_mesajlar 5");
+				for (var i = 0; i < a.length; i++) {
+					console.log("div_mesajlar 6");
+					$('#prj_' + a[i].campain_id).bind('tap',
+					function(event, ui) {
+						var strID = $(this).attr('id').replace('prj_','');
+						app.getProductsDetay(strID);
+					});
+				}
+
+		    },
+			error : function(a, b, c) {
+				$("#device_info").append('hata aldı '+ '<br />');
+				element2.innerHTML = "hata username:";
+
+				console.log("err a ", a);
+				console.log("err b ", b);
+				console.log("err c ", c);
+				console.log("err c ", c);
+			}
+		});		    
+		
+		
+		
+		$.ajax({
+			url : app.url+"GetCampains?conn_type=setread_campain_all&member_id="+app.id,
+			dataType : "json",
+			success : function(a, b, c) {
+				console.log("kampanyalar update");
+		    },
+			error : function(a, b, c) {
+				$("#device_info").append('hata aldı '+ '<br />');
+
+				console.log("err a ", a);
+				console.log("err b ", b);
+				console.log("err c ", c);
+				console.log("err c ", c);
+			}
+		});				    
 	},	
     fnc_Profil : function() {
 				$("#un_profil").empty();
 		        $("#un_profil").append(app.user_name+ "("+app.total_points+")");
-		        $("#txt_isim").val(app.name +" "+ app.surname);
+		        $("#txt_isim").val(app.name);
+		        $("#txt_soyisim").val(app.surname);
 		        $("#tx_tckn").val(app.identityno);
 		        $("#txt_dogumtarihi").val(app.birthdate);
 				$("#txt_cep_tel").val(app.mobile);
 				$("#txt_email").val(app.email);
 				$("#txt_adres").val(app.address_text);
 				if(app.allow_email==1)
-				$('#chk_mail').Attr('checked',true);
+				$('#chk_mail').prop('checked',true);
 				else
-				$('#chk_mail').Attr('checked',false);
+				$('#chk_mail').prop('checked',false);
 				
 				if(app.allow_sms==1)
-				$('#chk_sms').Attr('checked',true);
+				$('#chk_sms').prop('checked',true);
 				else
-				$('#chk_sms').Attr('checked',false);
+				$('#chk_sms').prop('checked',false);
 
 	},
 	fnc_Kampanyalar : function() {
@@ -237,14 +308,10 @@ var app = {
 				$("#un_istatistik").empty();
 		        $("#un_istatistik").append(app.user_name+ "("+app.total_points+")");
 	},
-	fnc_EnYakin : function() {
-				// $("#map").empty();
-		        // $("#map").append(app.user_name+ "("+app.total_points+")");
-	},			
-	member_savefunc : function() {
-	},	
-	getMusteriler : function(){
-	
+	fnc_Enyakin : function() {
+				$("#un_enyakin").empty();
+		        $("#un_enyakin").append(app.user_name+ "("+app.total_points+")");
+		        app.detectCurrentLocation();
 	},
 	isnull : function(p){
 		if (p ==null)
@@ -253,12 +320,49 @@ var app = {
 		return p;
 	},
 	first_init : function(){
-		app.uuid = app.isnull(device.uuid);
-		//if (app.uuid==".")
+		try{
+				app.uuid = app.isnull(device.uuid);
+		   }catch(err) 
+		   {
+		   		console.log(err.message);
+		   }
+
 		app.uuid="586BC0F6-09DC-44FB-8F1D-A3ABCB8E0C80";
-		app.user_name="Merhaba : Ayşe Balcı";
+		app.user_name="Merhaba : ";
 		app.user_id="90910000001";
 		app.id="123456789";
+
+
+		$.ajax({
+			url : app.url+"GetCampains?member_id="+app.id,
+			dataType : "json",
+			success : function(a, b, c) {
+				console.log("kampanyalar");
+				isThereNewCampain=false;
+				for (var i = 0; i < a.length; i++) {
+					if ((a[i].isread=="0") || (a[i].isread=="null"))
+					{
+						console.log("div_kapmanya isread = " + a[i].isread + "  a[i].company_id="+a[i].company_id);
+				    	isThereNewCampain=true;
+				    	if (a[i].company_id=="2") // aeropostale
+				    	  icon2=true;
+				    	if (a[i].company_id=="3") //bananerepuvlic
+				    	  icon3=true;
+						if (a[i].company_id=="22") //gap
+				    	  icon22=true;				    	  
+						if (a[i].company_id=="27") //mark spencer
+				    	  icon27=true;				    	  
+				    }
+				};
+		    },
+			error : function(a, b, c) {
+				$("#device_info").append('hata aldı '+ '<br />');
+				console.log("err a ", a);
+				console.log("err b ", b);
+				console.log("err c ", c);
+				console.log("err c ", c);
+			}
+		});		
 
 		$("#un_barkod").empty();
 		$("#un_barkod").append(app.user_name);
@@ -301,7 +405,7 @@ var app = {
 				  app.name=a.name;
 				  app.surname=a.surname;
 				  app.birthdate=a.birthdate;
-				  app.Birth_place=a.Birth_place;
+				  app.birth_place=a.Birth_place;
 				  app.address_type=a.address_type;
 				  app.address_text=a.address_text;
 				  app.city_id=a.city_id;
@@ -331,6 +435,107 @@ var app = {
 		}
 
 
+	//app.setbadge('#m1 a#msj span.badge', 0);
+	
+	if (isThereNewCampain)
+	{
+		console.log($("#img_msg").attr('src'));
+		$("#img_msg").attr("src","img/menu_icons/3_Message_y.png");
+		
+		console.log($("#img_camp").attr('src'));
+		$("#img_camp").attr("src","img/menu_icons/5_Campaign_y.png");
+
+		console.log($("#img_msg1").attr('src'));
+		$("#img_msg1").attr("src","img/menu_icons/3_Message_y.png");
+
+		console.log($("#img_camp1").attr('src'));
+		$("#img_camp1").attr("src","img/menu_icons/5_Campaign_y.png");
+
+
+		console.log($("#img_msg3").attr('src'));
+		$("#img_msg3").attr("src","img/menu_icons/3_Message_y.png");
+
+		console.log($("#img_camp3").attr('src'));
+		$("#img_camp3").attr("src","img/menu_icons/5_Campaign_y.png");
+
+		
+		console.log($("#img_msg5").attr('src'));
+		$("#img_msg5").attr("src","img/menu_icons/3_Message_y.png");
+
+		console.log($("#img_camp5").attr('src'));
+		$("#img_camp5").attr("src","img/menu_icons/5_Campaign_y.png");
+
+
+		console.log($("#img_msg6").attr('src'));
+		$("#img_msg6").attr("src","img/menu_icons/3_Message_y.png");
+
+		console.log($("#img_camp6").attr('src'));
+		$("#img_camp6").attr("src","img/menu_icons/5_Campaign_y.png");
+
+	}
+	else
+	{		
+		console.log($("#img_msg").attr('src'));
+		$("#img_msg").attr("src","img/menu_icons/3_Message.png");
+
+		console.log($("#img_camp").attr('src'));
+		$("#img_camp").attr("src","img/menu_icons/5_Campaign.png");
+
+
+		console.log($("#img_msg1").attr('src'));
+		$("#img_msg1").attr("src","img/menu_icons/3_Message.png");
+
+		console.log($("#img_camp1").attr('src'));
+		$("#img_camp1").attr("src","img/menu_icons/5_Campaign.png");
+
+
+		console.log($("#img_msg3").attr('src'));
+		$("#img_msg3").attr("src","img/menu_icons/3_Message.png");
+
+		console.log($("#img_camp3").attr('src'));
+		$("#img_camp3").attr("src","img/menu_icons/5_Campaign.png");
+
+		
+		console.log($("#img_msg5").attr('src'));
+		$("#img_msg5").attr("src","img/menu_icons/3_Message.png");
+
+		console.log($("#img_camp5").attr('src'));
+		$("#img_camp5").attr("src","img/menu_icons/5_Campaign.png");
+
+
+		console.log($("#img_msg6").attr('src'));
+		$("#img_msg6").attr("src","img/menu_icons/3_Message.png");
+
+		console.log($("#img_camp6").attr('src'));
+		$("#img_camp6").attr("src","img/menu_icons/5_Campaign.png");
+
+	}	
+
+// aeropostale
+if (icon2)
+{
+		console.log($("#icon2").attr('src'));
+		$("#icon2").attr("src","img/company_icons/2_.jpg");
+}
+
+//bananerepuvlic
+if (icon3)
+{
+		console.log($("#icon3").attr('src'));
+		$("#icon3").attr("src","img/company_icons/3_.jpg");
+}
+//gap
+if (icon22)
+{
+		console.log($("#icon22").attr('src'));
+		$("#icon22").attr("src","img/company_icons/22_.jpg");
+}				    	  
+//mark spencer
+if (icon27)
+{
+		console.log($("#icon27").attr('src'));
+		$("#icon27").attr("src","img/company_icons/27_.jpg");
+}
 	},
 	insertfunc : function() {
 		console.log("save func");
@@ -369,15 +574,41 @@ var app = {
 		var result= $("#sel_personels option:selected").val();
 		var result2= $("#sel_status option:selected").val();
 		var desc= $("#userDesc").val();
-		//if(app.status==null)
+		
+		        app.name = $("#txt_isim").val();
+		        app.surname= $("#txt_soyisim").val();
+		        app.identityno=$("#tx_tckn").val();
+		        app.birthdate=$("#txt_dogumtarihi").val();
+		        //app.birth_place=$("#txt_dogumyeri").val();
+				app.mobile=$("#txt_cep_tel").val();
+				app.email=$("#txt_email").val();
+				app.address_text=$("#txt_adres").val();
+
+				if ($('#chk_mail').is(":checked"))
+					app.allow_email=1;
+				else
+					app.allow_email=0;
+				
+				if ($('#chk_sms').is(":checked"))
+					app.allow_sms=1;
+				else
+					app.allow_sms=0;
+		
+		app.user_name ="Merhaba : " +app.name  + " "+app.surname;
+		//identityno=1234567890&name=selim&surname=göktaş&birthdate=1999&Birth_place=istanbul&address_type=1&address_text=adres&city_id=34&allow_email=0&allow_sms=1&mobile=5362798531&work_phone=536123456&home_phone=5363213232&fax=5363213232&email=selimgoktas@gtech.com.tr
 		{
-		$.ajax({			
-			url : app.url+"/istakip_yesis_webservices/GetMyActivities?android_id="+app.uuid+"&jsonType=1&con_type=updateactivity&temp_activity_type_id="+app.id+"&temp_status_id="+result2+"&temp_assignto="+result + "&desc=" + desc,
+		$.ajax({
+			url : app.url+"/GetMember?member_id="+app.id+
+			"&conn_type=update&identityno="+app.identityno+"&name="+app.name+"&surname="+app.surname+"&birthdate="+app.birthdate+
+			"&Birth_place="+app.birth_place+"&address_type="+app.address_type+"&address_text="+app.address_text+
+			"&city_id="+app.city_id+"&allow_email="+app.allow_email+
+			"&allow_sms="+app.allow_sms+"&mobile="+app.mobile+
+			"&work_phone="+app.work_phone+"&home_phone="+
+			app.home_phone+"&fax="+app.fax+"&email="+app.email ,
 			dataType : "json",
 			success : function(a, b, c) {
-			app.status=a;			
-			$.mobile.changePage($('#benim'));
-			app.getProducts2();
+				console.log( "başarılı" );
+			$.mobile.changePage($('#barkod'));
 			},
 			error : function(a, b, c) {
 				console.log("err a ", a);
@@ -412,7 +643,7 @@ var app = {
 		});
 	},
 
-	detectCurrentLocation : function() {
+detectCurrentLocation : function() {
         var onGeoSuccess = function(position) {
             console.log(position);
         
